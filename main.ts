8d2e4d8321fce80213cb90b433b39a380ffacebc
grperry <92124381+grperry@users.noreply.github.com>
2021-10-09 17:43:30 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,6 @@ input.onButtonPressed(Button.A, function () {
 })
 IotLoRaNode.InitialiseRadioOTAA("70B3D57ED004665C", "0000000000000000", "30347FB416A4352E5C6A3BC13EE102DE")
 loops.everyInterval(60000, function () {
-    IotLoRaNode.LightValue(input.lightLevel(), Channels.One)
+    IotLoRaNode.LightValue(input.lightLevel(), Channels.Two)
     IotLoRaNode.loraTransmitPayload()
 })

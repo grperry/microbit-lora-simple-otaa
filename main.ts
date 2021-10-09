a90b7da68e14a8b79469fa27400fc0ffd0e6e9f9
grperry <92124381+grperry@users.noreply.github.com>
2021-10-09 07:19:52 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    IotLoRaNode.DigitalValue(true, Channels.One)
+    IotLoRaNode.loraTransmitPayload()
+})
+IotLoRaNode.InitialiseRadioOTAA("70B3D57ED004665C", "0000000000000000", "30347FB416A4352E5C6A3BC13EE102DE")
+loops.everyInterval(60000, function () {
+    IotLoRaNode.LightValue(input.lightLevel(), Channels.One)
+    IotLoRaNode.loraTransmitPayload()
+})
